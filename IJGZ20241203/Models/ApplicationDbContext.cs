0009577d11fdb3a9104813d5c93924df65495529
IGabriel30 <ingridgabriel30@hotmail.com>
2024-03-12 12:53:34 -0600

    Clase DbContext, cadena de conexión e inyeccion de dependencias.

---
+++ IJGZ20241203/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IJGZ20241203.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Proveedor> Proveedors { get; set; }
+
+        public DbSet<DireccionProveedor> Direcciones { get; set; }
+
+    }
+}
