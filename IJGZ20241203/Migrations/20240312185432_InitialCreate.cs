4db308a5146da0d314ba27d69f16da3ed5615c1b
IGabriel30 <ingridgabriel30@hotmail.com>
2024-03-12 12:57:24 -0600

    Migracion

---
+++ IJGZ20241203/Migrations/20240312185432_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IJGZ20241203.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proveedors",
+                columns: table => new
+                {
+                    IdProveedor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Producto = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proveedors", x => x.IdProveedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Direcciones",
+                columns: table => new
+                {
+                    IdDireccion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProveedorId = table.Column<int>(type: "int", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Pais = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Direcciones", x => x.IdDireccion);
+                    table.ForeignKey(
+                        name: "FK_Direcciones_Proveedors_ProveedorId",
+                        column: x => x.ProveedorId,
+                        principalTable: "Proveedors",
+                        principalColumn: "IdProveedor",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Direcciones_ProveedorId",
+                table: "Direcciones",
+                column: "ProveedorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Direcciones");
+
+            migrationBuilder.DropTable(
+                name: "Proveedors");
+        }
+    }
+}
