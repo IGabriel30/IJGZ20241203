f67bfd81675ae5790fda4a6eaad1da41c9e1a32a
IGabriel30 <ingridgabriel30@hotmail.com>
2024-03-12 12:01:36 -0600

    Agregar archivos de proyecto.

---
+++ IJGZ20241203/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IJGZ20241203.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
