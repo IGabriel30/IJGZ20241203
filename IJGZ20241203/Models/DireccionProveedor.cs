e636fa2bab77c52ea372dc6fd717a71e08777a92
IGabriel30 <ingridgabriel30@hotmail.com>
2024-03-12 12:33:50 -0600

    Creación de modelos

---
+++ IJGZ20241203/Models/DireccionProveedor.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IJGZ20241203.Models
+{
+    public class DireccionProveedor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdDireccion { get; set; }
+        public int ProveedorId {  get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(200,ErrorMessage ="200 caracteres maximo")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El campo {} es requerido")]
+        [MaxLength(30, ErrorMessage ="Máximo 30 caracteres")]
+        public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El campo País es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        public string Pais { get; set; }
+    }
+}
---
+++ IJGZ20241203/Models/Proveedor.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IJGZ20241203.Models
+{
+    public class Proveedor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdProveedor { get; set; }
+
+        [Required(ErrorMessage ="El campo {} es requerido")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage ="El campo { } es requerido")]
+        [Phone(ErrorMessage ="Digíte un número de teléfono valido")]
+        public string Telefono {  get; set; }
+
+        [EmailAddress(ErrorMessage ="Digíte un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo { } es requerido")]
+        [MaxLength(75, ErrorMessage ="Máximo 75 caracteres")]
+        public string Producto { get; set; }
+
+
+        [Required(ErrorMessage = "El campo { } es requerido")]
+        [Display(Name ="Fecha de registro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+}
